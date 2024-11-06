77756bf02c8c2eb4c42a32ed8eb4c539a127eb74
Affan Sharief <affansharief999@gmail.com>
2024-11-06 09:23:01 +0530

    directory structure & component creation

---
+++ ticketing/src/app/core/constants/Constants.ts
@@ -0,0 +1,11 @@
+export const Constant = {
+    API_ENDPOINT: {
+        GET_DEPARTMENT: 'GetDepartments',
+        CREATE_DEPARTMENT: 'CreateDepartment',
+        UPDATE_DEPARTMENT: 'UpdateDepartment',
+        DELETE_DEPARTMENT: 'DeleteDepartment?id='
+    },
+    VALIDATION_MESSAGE: {
+        REQUIRED: 'This is Required'
+    }
+}
