6864f6fb45db801149fe07268128b4dbc16a2fa3
Affan Sharief <affansharief999@gmail.com>
2024-11-23 15:59:38 +0530

    Employee Component FED

--- ticketing/src/app/pages/employee/employee.component.ts
+++ ticketing/src/app/pages/employee/employee.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DepartmentService } from '../../core/services/department.service';
+import { map, Observable, pipe } from 'rxjs';
+import { APIResponse, Department, EmployeeModel } from '../../core/models/API.model';
+import { CommonModule } from '@angular/common';
+import { EmployeeService } from '../../core/services/employee.service';
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
-export class EmployeeComponent {
+export class EmployeeComponent implements OnInit {
+  allDept$!: Observable<Department[]>;
+  allEmployees$!: Observable<EmployeeModel[]>;
+  isEmpForm: boolean = false;
+  constructor(
+    private deptService: DepartmentService,
+    private empService: EmployeeService
+  ){}
+
+  ngOnInit(): void {
+    this.allDept$ = this.deptService.getAllDept().pipe(map((res: APIResponse) => {
+      return res.data;
+    }));
+
+    this.allEmployees$ = this.empService.getAllEmployees().pipe(
+      map((res: APIResponse) => {
+        return res.data;
+      })
+    );
+  }
 
 }
