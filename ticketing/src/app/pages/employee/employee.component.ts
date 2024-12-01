6dccc0c522f9d3b2b5097b4185fc1c7ff599780e
Affan Sharief <affansharief999@gmail.com>
2024-12-01 17:23:25 +0530

    employee form submit

--- ticketing/src/app/pages/employee/employee.component.ts
+++ ticketing/src/app/pages/employee/employee.component.ts
@@ -4,12 +4,16 @@ import { map, Observable, pipe } from 'rxjs';
 import { APIResponse, Department, EmployeeModel } from '../../core/models/API.model';
 import { CommonModule } from '@angular/common';
 import { EmployeeService } from '../../core/services/employee.service';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-employee',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
@@ -18,10 +22,22 @@ export class EmployeeComponent implements OnInit {
   allDept$!: Observable<Department[]>;
   allEmployees$!: Observable<EmployeeModel[]>;
   isEmpForm: boolean = false;
+  empForm!: FormGroup;
   constructor(
     private deptService: DepartmentService,
-    private empService: EmployeeService
-  ){}
+    private empService: EmployeeService,
+    private fb: FormBuilder
+  ){ 
+    this.empForm = this.fb.group({
+      name: ['', [Validators.required]],
+      contact: ['', [Validators.required, Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$')]],
+      email: ['', [Validators.required, Validators.email]],
+      empGender: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      dept: ['', [Validators.required]],
+      role: ['', [Validators.required]]
+    });
+  }
 
   ngOnInit(): void {
     this.allDept$ = this.deptService.getAllDept().pipe(map((res: APIResponse) => {
@@ -33,6 +49,19 @@ export class EmployeeComponent implements OnInit {
         return res.data;
       })
     );
+  }
+
+  resetEmpForm(){
+    this.empForm.reset();
+  }
+
+  saveEmployee(){
+    console.log(this.empForm.value);
+    if(this.empForm.valid){
+      alert('Employee Created Successfully!!!');
+    }else{
+      alert('Enter Valid Data!!!');
+    }
   }
 
 }
